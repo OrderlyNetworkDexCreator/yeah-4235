f0d457daac7b38a196c4821a6336e697a17bddd3
Mario Reder <mario.reder@pm.me>
2025-10-08 22:32:50 +0200

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -19,7 +19,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_HAS_SECONDARY_LOGO": "false",
   "VITE_ENABLED_MENUS": "Trading,Markets,Portfolio,Vaults,Rewards,Leaderboard",
   "VITE_CUSTOM_MENUS": "Docs, https://docs.yeah.com",
-  "VITE_ENABLE_CAMPAIGNS": "false",
+  "VITE_ENABLE_CAMPAIGNS": "true",
   "VITE_TELEGRAM_URL": "",
   "VITE_DISCORD_URL": "",
   "VITE_TWITTER_URL": "",
